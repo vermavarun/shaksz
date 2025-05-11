c662410e1e83307e69bc5c16bf28f5f4e825edb0
Varun Verma <vermavarun@outlook.com>
2025-05-11 16:56:21 +0530

    Add threeConsecutiveOdds function and corresponding test case

--- shaksz/leetcode.py
+++ shaksz/leetcode.py
@@ -11,3 +11,20 @@ def twoSum(nums, target):
             return [hash_map[difference], i]
           hash_map[num] = i
       return []
+
+'''
+LeetCode Problem 1550. Three Consecutive Odds
+Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+'''
+def threeConsecutiveOdds(arr):
+    """
+    :type arr: List[int]
+    :rtype: bool
+    """
+    size = len(arr)                                                         # Get the size of the array
+    if size <=2:                                                            # If size is less than or equal to 2, return False
+        return False                                                        # Since it's impossible to have three consecutive odds
+    for i in range(size - 2):                                               # Iterate through the array
+        if arr[i] % 2 != 0 and arr[i+1] % 2 != 0 and arr[i+2] % 2 != 0:     # Check if three consecutive numbers are odd
+            return True                                                     # If found, return True
+    return False                                                            # If no such triplet is found, return False
--- tests/test_leetcode.py
+++ tests/test_leetcode.py
@@ -7,6 +7,9 @@ class TestModule(unittest.TestCase):
     def test_twoSum(self):
         result = leetcode.twoSum([2,7,11,15],9)
         self.assertEqual(result, [0,1])
+    def test_threeConsecutiveOdds(self):
+        result = leetcode.threeConsecutiveOdds([2,6,4,1])
+        self.assertEqual(result, False)
 
 if __name__ == '__main__':
     unittest.main()
